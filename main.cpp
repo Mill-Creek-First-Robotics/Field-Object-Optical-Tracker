e90025f8fa6c8ad2e157a8626565ddefcc5a73e1
Chris <cheesecakecgcontact@gmail.com>
2020-01-25 22:06:56 -0500

    Displays camera image

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 using namespace cv;
-int main(int argc, char** argv )
+int main(int argc, char** argv)
 {
-    if ( argc != 2 )
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+    for(;;)
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+          Mat frame;
+          cap >> frame;
+          if( frame.empty() ) break; // end of video stream
+          imshow("this is you, smile! :)", frame);
+          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
     }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+    // the camera will be closed automatically upon exit
+    // cap.close();
     return 0;
 }
